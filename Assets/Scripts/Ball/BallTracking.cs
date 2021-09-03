2802466cb11d2042dfe5adba07755d0d407bd50f
TrendDead <icykot5hi@gmail.com>
2021-09-03 18:16:12 +0700

    add HelixJump

---
+++ Assets/Scripts/Ball/BallTracking.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTracking : MonoBehaviour
+{
+    [SerializeField] private Vector3 _directionOffset;
+    [SerializeField] private float _lenght;
+
+    private Ball _ball;
+    private Beam _beam;
+    private Vector3 _cameraPosition;
+    private Vector3 _minimalBallPosition;
+
+    private void Start()
+    {
+        _ball = FindObjectOfType<Ball>();
+        _beam = FindObjectOfType<Beam>();
+
+        _cameraPosition = _ball.transform.position;
+        _minimalBallPosition = _ball.transform.position;
+
+        TrackBall();
+    }
+
+    private void Update()
+    {
+        if(_ball.transform.position.y < _minimalBallPosition.y)
+        {
+            TrackBall();
+            _minimalBallPosition = _ball.transform.position;
+        }
+    }
+
+    private void TrackBall()
+    {
+        Vector3 beamPosition = _beam.transform.position;
+        beamPosition.y = _ball.transform.position.y;
+        _cameraPosition = _ball.transform.position;
+        Vector3 direction = (beamPosition - _ball.transform.position).normalized + _directionOffset;
+        _cameraPosition -= direction * _lenght;
+        transform.position = _cameraPosition;
+        transform.LookAt(_ball.transform);
+    }
+}
